356f3df0a63741018e46e58e0a6cf0c8adbd091b
binnej <meneer.binnemans@gmail.com>
2026-01-02 17:36:03 +0100

    main.blocks, custom.ts bijwerken

--- custom.ts
+++ custom.ts
@@ -4,11 +4,12 @@
 * Lees meer op https://makecode.microbit.org/blocks/custom
 */
 
-enum MyEnum {
-    //% block="one"
+enum lichten_status {
+    //% block="Rood"
     Rood,
-    //% block="two"
+    //% block="Oranje"
     Oranje,
+    //% block="Groen"
     Groen
 }
 
@@ -18,14 +19,26 @@ enum MyEnum {
 //% weight=100 color=#0fbc11 icon=""
 namespace Verkeerslicht {
     /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
+     * Met dit blokje kan je de verkeerslichten aansturen. 
+     * @param e De kleur van de lamp die op het verkeerslicht moet aangaan. 
      */
     //% block
-    export function foo(e: MyEnum): void {
-        // Add code here
+    export function foo(e: lichten_status): void {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+
+        if (e == lichten_status.Rood) {
+            pins.digitalWritePin(DigitalPin.P0, 1)
+        }
+        if (e == lichten_status.Oranje) {
+            pins.digitalWritePin(DigitalPin.P1, 1)
+        }
+        if (e == lichten_status.Groen) {
+            pins.digitalWritePin(DigitalPin.P2, 1)
+        }
+
+
     }
 
     /**
